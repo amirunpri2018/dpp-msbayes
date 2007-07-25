34f0bb7775061c28216e6d2573e3e42498d4b748
mhickerson <mhickerson@9521fd22-b02c-0410-b470-ef1aee0140e0>
2007-07-25 20:28:51 +0000

    Added subpopulation summary statistics
    git-svn-id: https://msbayes.svn.sourceforge.net/svnroot/msbayes/trunk@9 9521fd22-b02c-0410-b470-ef1aee0140e0

--- src/msStatsDQH.c
+++ src/msStatsDQH.c
@@ -34,8 +34,11 @@
 
 double nucdiv (int, int, char **);
 double nucdiv_w (int, int, char **, int, int *);
+double nucdiv_w1(int, int, char **, int, int *);
+double nucdiv_w2(int, int, char **, int, int *);
 double nucdiv_bw (int, int, char **, int, int *);
 double tajddenominator (int, int, double);
+double tajddenominator2(int, int, double) ;
 double thetaW (int, int);
 double thetah (int, int, char **);
 void FuLi (double *D, double *F, int, int, char **, double pi);
@@ -43,6 +46,7 @@ void FrequencyDistrInfSites (int *freqdist, int nsam, int segsites,
 			     char **list);
 void FrequencyDistrQ (int *freqdist, int nsam, int segsites, char **list);
 int segsub (int nsub, int segsites, char **list);
+void segsubs( int *segwithin, int segsites, char **list, int npops, int *n ); /*yyy Eli 05/15/06 yyy*/
 int multiplepopssampledfrom (int nsam, int npops, int *n);	/*zzz n[] is called config[] in samples.c zzz */
 static int SS_comp (const void *, const void *);
 #if 0
@@ -50,14 +54,22 @@ static int compare_doubles (const void *a, const void *b);
 #endif
 
 struct SumStat
-{
-  double PI_Net;
-  double PI_b;
-  double PI_w;
-  double PI;
-  double TD;
-  double TW;
-  double TDD;
+{	
+	double PI_b  ;
+	double PI_Net  ;
+	double TD ;
+	double TD1 ;
+	double TD2 ;
+	double PI_w ;
+	double PI_w1 ;
+	double PI_w2 ;
+	double PI ;
+	double TW ;
+	double TW1 ;
+	double TW2 ;
+	double TDD ;
+	double TDD1 ;
+	double TDD2 ;
 };
 
 #if 0  /* commented out since it is not used */
@@ -85,8 +97,8 @@ static int SS_comp (const void *p1, const void *p2)
   const struct SumStat *sp1 = (struct SumStat *) p1;
   const struct SumStat *sp2 = (struct SumStat *) p2;
 
-  return ((sp1->PI_Net) > (sp2->PI_Net)) - ((sp1->PI_Net) <
-					    (sp2->PI_Net));
+  return ((sp1->PI) > (sp2->PI)) - ((sp1->PI) <
+					    (sp2->PI));
   /*return  ( sp1->PI_b) - ( sp2->PI_b); */
 }
 
@@ -96,13 +108,15 @@ printstats (int nsam, int segsites, char **list, int nsub, int npops, int *n,
 	    double THETA, int Sbool, int Qbool, int Fst_bool, double TAU,
 	    int count, int TAXAcount, int BasePairs, int TauHyp, int NumTaxa)
 {
-  int  STATLOAD, SSLOAD;
-  double tW, pi, D,  FuLiD, FuLiF, CV, TDen;
-  double pi_w=-1, pi_b=-1, Fst, Nm, Pi_Net = -1;/*zz7z Hickerson 7/29/04 zzz */
+  int  i, STATLOAD, SSLOAD;
+  double tW2,tW, tW1, pi, D, D1, D2,  FuLiD, FuLiF, CV, TDen, TDen2, TDen1;
+  double pi_w2=-1, pi_w1=-1,  pi_w=-1, pi_b=-1, Fst, Nm, Pi_Net = -1;/*zz7z Hickerson 7/29/04 zzz */
   int *freqdist, nsegsub=-1, CC, a;
   double NSEGSUB[NumTaxa];
-  double TW[NumTaxa], PI[NumTaxa], PI_w[NumTaxa], PI_b[NumTaxa], 
-    PI_Net[NumTaxa], TD[NumTaxa], tau[NumTaxa], TDD[NumTaxa];
+  double TW[NumTaxa],TW1[NumTaxa],TW2[NumTaxa], PI[NumTaxa], PI_w[NumTaxa], PI_w1[NumTaxa],PI_w2[NumTaxa],PI_b[NumTaxa], 
+    PI_Net[NumTaxa], TD[NumTaxa],TD1[NumTaxa],TD2[NumTaxa], tau[NumTaxa], TDD[NumTaxa], TDD1[NumTaxa], TDD2[NumTaxa];
+      double tW_w;/*zzz Hickerson 6/26/04 zzz */ /*yyy tW_w  Eli 05/15/06 yyy*/
+int *segwithin, tW_w_npops ;/*yyy Eli 5/15/06 yyy*/
   double MeanTAU, VarTAU;
   /* double h, th, ObsvVARD, ObsvVARPi_Net, ObsvEPi_Net, ObsvCV_pi_net_tW; */
 
@@ -132,7 +146,21 @@ printstats (int nsam, int segsites, char **list, int nsub, int npops, int *n,
   /*  printf("%d\t%d\n", npops, n); */
   if (Fst_bool)
     {
+	      /*yyy BELOW  Eli 05/15/06 yyy*/
+    segwithin = (int *)malloc( npops *sizeof(int));
+    segsubs( segwithin, segsites, list, npops, n) ;
+    tW_w=0.;
+    tW_w_npops=0;
+    for (i=0;i<npops;i++)
+      if(n[i]>1) {tW_w+=thetaW(n[i],segwithin[i]); tW_w_npops++;}
+    tW_w/=tW_w_npops;
+    /*yyy ABOVE  Eli 05/15/06 yyy*/
+	    tW2 = (double) thetaW(n[1], segwithin[1]);
+		tW1 = (double) thetaW(n[0], segwithin[0]);
       pi_w = (double) nucdiv_w (nsam, segsites, list, npops, n) / BasePairs;
+	  pi_w2 = (double) nucdiv_w2(nsam, segsites, list, npops, n);
+    pi_w1 = (double) nucdiv_w1(nsam, segsites, list, npops, n);
+
       pi_b = (double) nucdiv_bw (nsam, segsites, list, npops, n) / BasePairs;
       Fst = 1. - pi_w / pi_b;
       Pi_Net = (double) pi_b - pi_w;
@@ -146,6 +174,10 @@ printstats (int nsam, int segsites, char **list, int nsub, int npops, int *n,
     }				/*zzz Hickerson 7/29/04 zzz */
 /*   th = thetah(nsam, segsites, list) ; */
   D = (pi - tW) / tajddenominator (nsam, segsites, pi);
+      D2 = (pi_w2-tW2)/tajddenominator2(n[1],segwithin[1],pi_w2) ;
+	D1 = (pi_w1-tW1)/tajddenominator2(n[0],segwithin[0],pi_w2) ;
+		TDen2 =tajddenominator2(n[1],segwithin[1],pi_w2) ;
+		TDen1 =tajddenominator2(n[0],segwithin[0],pi_w1) ;
   TDen = tajddenominator (nsam, segsites, pi);
 /*  FuLi(&FuLiD,&FuLiF,nsam,segsites,list,pi);*/
 /*   h = pi-th ; */
@@ -153,12 +185,16 @@ printstats (int nsam, int segsites, char **list, int nsub, int npops, int *n,
     nsegsub = segsub (nsub, segsites, list);
 
 
-  tW = tW / BasePairs;
-  pi = pi / BasePairs;
+  tW2 =   tW2/BasePairs;
+  tW1 =   tW1/BasePairs;
+  tW =   tW/BasePairs;
+  pi =  pi/BasePairs;
+	pi_w2 = pi_w2/BasePairs;
+	pi_w1 = pi_w1/BasePairs; 
 
-  if (segsites < 1)
-    D = 0, Fst = 0, Pi_Net = 0, FuLiD = 0, FuLiF = 0, tW = 0, pi = 0, pi_b =
-      0, pi_w = 0, TDen = 0;
+if (segsites < 1) D=0, Fst=0, Pi_Net=0, FuLiD=0, FuLiF=0, tW=0,tW1=0,tW2=0, pi=0, pi_b=0, pi_w=0, pi_w1=0, pi_w2=0, TDen =0,TDen1 =0,TDen2 =0;
+if (segwithin[1]<1 ) D2=0;
+if (segwithin[0]<1 ) D1=0;
   if (Pi_Net < 0)
     Pi_Net = 0;
   if (pi_b < 0)
@@ -178,8 +214,8 @@ printstats (int nsam, int segsites, char **list, int nsub, int npops, int *n,
   /*fprintf(fp, "%lf\t%lf\t%lf\t%lf\t%lf\t%lf\t%lf\n", pi_b, FuLiD, FuLiF, tW, pi, segsites, pi_w, Fst, Pi_Net, D, TAU, TDen);
      fclose (fp); */
 
-  fprintf (fp, "%lf\t%lf\t%lf\t%lf\t%lf\t%lf\t%lf\t%lf\n", pi_b, tW, pi, pi_w,
-	   Pi_Net, D, TDen, TAU);
+   fprintf(fp, "%lf\t%lf\t%lf\t%lf\t%lf\t%lf\t%lf\t%lf\t%lf\t%lf\t%lf\t%lf\t%lf\t%lf\t%lf\n", pi_w2, tW2, TDen2, pi_w1, tW1, TDen1, pi, tW, TDen, D2, D1, D, pi_b, Pi_Net, TAU );
+
   fclose (fp);
 
 
@@ -196,21 +232,27 @@ printstats (int nsam, int segsites, char **list, int nsub, int npops, int *n,
       STATLOAD = NumTaxa;
       SSLOAD = 11;
       for (a = 0; a < STATLOAD; a++)
-	fscanf (fp, "%lf\t%lf\t%lf\t%lf\t%lf\t%lf\t%lf\t%lf\n", &PI_b[a],
-		&TW[a], &PI[a], &PI_w[a], &PI_Net[a], &TD[a], &TDD[a],
-		&tau[a]);
+	fscanf(fp, "%lf\t%lf\t%lf\t%lf\t%lf\t%lf\t%lf\t%lf\t%lf\t%lf\t%lf\t%lf\t%lf\t%lf\t%lf\n", &PI_w2[a], &TW2[a], &TDD2[a], &PI_w1[a], &TW1[a], &TDD1[a], &PI[a], &TW[a], &TDD[a], &TD2[a], &TD1[a], &TD[a], &PI_b[a], &PI_Net[a], &tau[a]);
 
       fclose (fp);
 
       for (a = 0; a < STATLOAD; a++)
 	{
-	  SumStat_list[a].PI_Net = PI_Net[a];
-	  SumStat_list[a].PI_b = PI_b[a];
-	  SumStat_list[a].PI_w = PI_w[a];
-	  SumStat_list[a].PI = PI[a];
-	  SumStat_list[a].TD = TD[a];
-	  SumStat_list[a].TW = TW[a];
-	  SumStat_list[a].TDD = TDD[a];
+		SumStat_list[a].PI_b = PI_b[a];
+	 	SumStat_list[a].PI_Net = PI_Net[a];
+	SumStat_list[a].TD2 = TD2[a];
+	SumStat_list[a].TD1 = TD1[a];
+	SumStat_list[a].TD = TD[a];
+	SumStat_list[a].PI_w = PI_w[a];
+	SumStat_list[a].PI_w2 = PI_w2[a];
+	SumStat_list[a].PI_w1 = PI_w1[a];
+	SumStat_list[a].PI = PI[a];
+	SumStat_list[a].TW = TW[a];
+	SumStat_list[a].TW2 = TW2[a];
+	SumStat_list[a].TW1 = TW1[a];
+	SumStat_list[a].TDD = TDD[a];
+	SumStat_list[a].TDD2 = TDD2[a];
+	SumStat_list[a].TDD1 = TDD1[a];
 	}
  
       qsort (SumStat_list, NumTaxa, sizeof SumStat_list[0], SS_comp);
@@ -225,29 +267,43 @@ printstats (int nsam, int segsites, char **list, int nsub, int npops, int *n,
 	/*for (a=0;a<STATLOAD;a++)printf("%lf\t", SumStat_list[a].PI_Net);
 	   printf("\n"); */
 
-	for (a = 0; a < STATLOAD; a++)
-	  printf ("%lf\t", SumStat_list[a].PI_b);
+	/*for (a = 0; a < STATLOAD; a++)
+	  printf ("%lf\t", SumStat_list[a].PI_b);*/
 
 	for (a = 0; a < STATLOAD; a++)
-	  printf ("%lf\t", SumStat_list[a].PI_w);
+	  printf ("%lf\t", SumStat_list[a].PI_w2);
+	  
+	  	for (a = 0; a < STATLOAD; a++)
+	  printf ("%lf\t", SumStat_list[a].PI_w1);
 
 	for (a = 0; a < STATLOAD; a++)
 	  printf ("%lf\t", SumStat_list[a].PI);
-
+	  
+	for (a = 0; a < STATLOAD; a++)
+	  printf ("%lf\t", SumStat_list[a].TW2);
+	  
+	  	for (a = 0; a < STATLOAD; a++)
+	  printf ("%lf\t", SumStat_list[a].TW1);
+	  
 	for (a = 0; a < STATLOAD; a++)
 	  printf ("%lf\t", SumStat_list[a].TW);
 
 	for (a = 0; a < STATLOAD; a++)
 	  printf ("%lf\t", SumStat_list[a].PI_Net);
 
-	for (a = 0; a < STATLOAD; a++)
-	  printf ("%lf\t", SumStat_list[a].TD);
+	/*for (a = 0; a < STATLOAD; a++)
+	  printf ("%lf\t", SumStat_list[a].TD);*/
 
 	for (a = 0; a < STATLOAD; a++)
+	  printf ("%lf\t", SumStat_list[a].TDD2);
+
+	for (a = 0; a < STATLOAD; a++)
+	  printf ("%lf\t", SumStat_list[a].TDD1);
+	
+		for (a = 0; a < STATLOAD; a++)
 	  printf ("%lf\t", SumStat_list[a].TDD);
 	printf ("\n");
-
-
+	
 	/*    if ((fp=fopen("likeout1_21", "a+b")) ==NULL){
 	   fprintf(stderr,"Cannot open the file.\n");
 	   exit(1);
@@ -320,6 +376,26 @@ pairwisediffc_w (int ss, int nsam, char **list, int np, int *n, int pop)
   return (diffc);
 }
 
+  int
+pairwisediffc_w12(int ss, int nsam, char **list, int np, int *n, int pop)
+{
+  int n1,n2,diffc=0;
+  int popi,startn=0;
+  int s;
+  if (n[pop]>1) {
+    /*    for (popi=0;popi<pop;popi++) startn += n[popi];*/    
+    for (popi=pop;popi<pop;popi++) startn += n[popi]; 
+    for(n1=startn;n1<(startn+n[pop]-1);n1++)
+      for(n2=n1+1;n2<(startn+n[pop]);n2++) {
+        for(s=0;s<ss;s++)
+          if (list[n1][s]!=list[n2][s]) diffc++;
+      }
+  }
+
+  return(diffc);
+}
+
+
 int
 pairwisediffc_b (int ss, int nsam, char **list, int np, int *n, int pop1,
 		 int pop2)
@@ -347,6 +423,75 @@ pairwisediffc_b (int ss, int nsam, char **list, int np, int *n, int pop1,
   return (diffc);
 
 }
+/*yyy BELOW  Eli 05/15/06 yyy*/
+	void
+segsubs( int *segw, int segsites, char **list, int np, int *n)
+{
+	int i, count, npi, n0=0, ni , npops_gt1=0 ;
+	
+
+   	for( npi = 0; npi <np; npi++){
+	  if (n[npi]>1) {
+	    ++npops_gt1 ;
+	    count=0;
+	    for(i=0; i < segsites ; i++){
+	      for (ni=n0+1;ni<n0+n[npi];ni++) {
+		if( list[ni][i] != list[n0][i]  ) { segw[npi]++; break; }
+	      }
+	    }
+	  } else {
+	    segw[npi]=0;
+	  }
+	  n0+=n[npi];
+	}
+}
+/*yyy ABOVE  Eli 05/15/06 yyy*/
+
+
+	double
+nucdiv_w2( int nsam, int segsites, char **list, int np, int *n)
+{
+	int pop, pairwisediffc_w12(int, int, char**, int, int *, int);
+	double pi, nd  ;
+	double num_comps;
+
+	pi = 0.0 ;
+
+	nd = nsam;
+	num_comps = 0.;
+   	pop=1;
+	pi = pairwisediffc_w12(segsites,nsam,list,np,n,pop) ;
+	/* printf("piW: %lf\n", pi);   */
+        num_comps = (double)n[pop]*((double)n[pop]-1.)/2. ;
+
+	pi /= num_comps;
+        /*printf("piW-FINAL: %lf\n", pi);  */
+	return( pi ) ;
+}
+
+        double
+nucdiv_w1( int nsam, int segsites, char **list, int np, int *n)
+{
+  int pop, pairwisediffc_w12(int, int, char**, int, int *, int);
+  double pi, nd  ;
+  double num_comps;
+
+  pi = 0.0 ;
+
+  nd = nsam;
+  num_comps = 0.;
+  pop=0;
+  pi = pairwisediffc_w12(segsites,nsam,list,np,n,pop) ;
+  /* printf("piW: %lf\n", pi);   */
+  num_comps = (double)n[pop]*((double)n[pop]-1.)/2. ;
+
+  pi /= num_comps;
+  /*printf("piW-FINAL: %lf\n", pi);  */
+  return( pi ) ;
+}
+
+
+
 
 double
 nucdiv_w (int nsam, int segsites, char **list, int np, int *n)
@@ -489,6 +634,26 @@ tajddenominator (int n, int S, double p)
   return (denom);
 }
 
+	double 
+tajddenominator2(int n, int S, double p)
+{
+  int i;
+  double n1,S1,a1,a2,b1,b2,e1,e2,denom;
+  n1=(double)n;
+  S1=(double)S;
+  a1=a2=0.;
+  for (i=1;i<n;i++) {
+    a1 += 1./i ;
+    a2 += 1./(i*i) ;
+  }
+  b1 = (n1+1)/(3.*(n1-1)) ;
+  b2 = 2.*((n1*n1)+n1+3)/(9.*n1*(n1-1)) ;
+  e1 = (b1-1./a1)/a1 ;
+  e2 = (b2-(n1+2.)/(a1*n1)+a2/(a1*a1)) /(a1*a1+a2) ;
+  denom=sqrt(e1*S1+e2*S1*(S1-1)) ;
+  return(denom);
+}
+
 
 double
 thetaW (int n, int S)
@@ -506,27 +671,56 @@ thetaW (int n, int S)
 
 
 /* Calcuate the average pairwise differences */
+
+/*
 double
 nucdiv (int nsam, int segsites, char **list)
 {
-  int s, frequency (char, int, int, char **);	/*7/27/04; Hickerson */
+  int s, frequency (char, int, int, char **);	
   double pi,  denom;
   char dummy = '?';
   pi = 0.0;
   for (s = 0; s < segsites; s++)
     {
-      /* 
-       * frequency() returns the number of pair wise differences
-       * at site s from all pairwise comparison
-       */
-      pi += frequency (dummy, s, nsam, list);	/*7/27/04; Hickerson */
+	
+
+	   
+      pi += frequency (dummy, s, nsam, list);	
     }
-  denom = nsam * (nsam - 1) / 2;   /* # of ways to choose a pair:
-				      nsam choose 2 */
+  denom = nsam * (nsam - 1) / 2;   
   pi = pi / denom;
-  /*printf("piFINAL: %lf\n", pi);  test print */
+
 
   return (pi);
+}	*/
+/* denomis  # of ways to choose a pair: nsam choose 2 */
+      /* frequency() returns the number of pair wise differences at site s from all pairwise comparison */
+
+        double
+nucdiv( int nsam, int segsites, char **list)
+{
+	int s, n, i, frequency(char, int, int, char**);/*7/27/04; Hickerson*/
+	double pi, p1, nd, nnm1, denom  ;
+        char  dummy;
+	pi = 0.0 ;
+        denom = 0.0;
+	nd = nsam;
+	nnm1 = nd/(nd-1.0) ;
+   	for( s = 0; s <segsites; s++){
+                /*printf("s: %d", s);*/
+		n=0;
+                   p1 = frequency(dummy, s, nsam,list) ; /*7/27/04; Hickerson*/
+                pi = pi + p1; /*7/27/04; Hickerson*/
+                /*printf("pi: %lf\n", pi);  test print*/  	
+                }
+                denom = 0.0;
+                for( i=0; i<nsam; i++){/*7/27/04; Hickerson*/
+                    denom = i + denom;
+                    }
+                    pi = pi/denom;
+                       /*printf("piFINAL: %lf\n", pi);  test print */
+                    
+	return( pi ) ;
 }
 
 
