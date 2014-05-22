a517e8a64d345a4e05b48a1db3f6b75f156990b7
Jamie Oaks <joaks1@gmail.com>
2014-05-22 13:34:24 -0700

    Updating `build.sh` script.
    Moving the build/test/install code into a function. And adding a hidden
    `--universal-linux-build` option that is not working yet on my ubuntu
    machine.

--- build.sh
+++ build.sh
@@ -12,14 +12,62 @@ usage () {
     echo ""
 }
 
+build_dpp_msbayes () {
+    cmake_args=$@
+    if [ -n "$DPP_MSBAYES_BASE_DIR" ]
+    then
+        base_dir="$DPP_MSBAYES_BASE_DIR"
+    else
+        echo "ERROR: DPP_MSBAYES_BASE_DIR was not defined prior to calling "
+        echo "build_dpp_msbayes."
+        exit 1
+    fi
+    if [ -n "$DPP_MSBAYES_BUILD_DIR" ]
+    then
+        build_dir="$DPP_MSBAYES_BUILD_DIR"
+    else
+        build_dir="${base_dir}/build"
+    fi
+    if [ -d "$build_dir" ]
+    then
+        echo "ERROR: build directory '$build_dir' already exists."
+        echo "To reconfigure, please remove this directory and re-run this script."
+        exit 1
+    else
+        mkdir "$build_dir"
+    fi
+    
+    # configure make files and build
+    cd "$build_dir"
+    echo "Configuring make files..."
+    echo "    "${base_dir}"/ $cmake_args | xargs cmake"
+    echo "${base_dir}/ $cmake_args" | xargs cmake || exit 1
+    echo "Building..."
+    make || exit 1
+    if [ -n "$DPP_MSBAYES_RUN_TESTS" ]
+    then
+        echo "Building and running test suite..."
+        make check || exit 1
+    fi
+    if [ -n "$DPP_MSBAYES_RUN_INSTALL" ]
+    then
+        echo "Installing..."
+        make install || exit 1
+    fi
+    cd "$base_dir"
+    echo "Done!"
+}
+
+
 # get location of script
+DPP_MSBAYES_BASE_DIR=""
 this_dir=`dirname "$0"`
 if [ "$this_dir" = "." ]
 then
-    base_dir="$(pwd)"
+    export DPP_MSBAYES_BASE_DIR="$(pwd)"
 else
     cd "$this_dir"
-    base_dir="$(pwd)"
+    export DPP_MSBAYES_BASE_DIR="$(pwd)"
 fi
 
 # make sure abacus is here and up to date
@@ -30,10 +78,11 @@ git submodule update
 extra_args=""
 static=""
 install_prefix=""
-run_tests=""
-run_install=""
+DPP_MSBAYES_RUN_TESTS=""
+DPP_MSBAYES_RUN_INSTALL=""
 include_old=""
 universal_mac_build=""
+universal_linux_build=""
 while [ "$1" != "" ]
 do
     case $1 in
@@ -49,10 +98,12 @@ do
             static=1
             ;;
         -t| --test)
-            run_tests=1
+            DPP_MSBAYES_RUN_TESTS=1
+            export DPP_MSBAYES_RUN_TESTS
             ;;
         -i| --install)
-            run_install=1
+            DPP_MSBAYES_RUN_INSTALL=1
+            export DPP_MSBAYES_RUN_INSTALL
             ;;
         --old)
             include_old=1
@@ -62,16 +113,17 @@ do
             include_old=1
             static=1
             ;;
+        --universal-linux-build)
+            universal_linux_build=1
+            include_old=1
+            static=1
+            ;;
         * )
             extra_args="$extra_args $1"
     esac
     shift
 done
 args=""
-if [ -n "$install_prefix" ]
-then
-    args="${args} -DCMAKE_INSTALL_PREFIX=${install_prefix}"
-fi
 if [ -n "$static" ]
 then
     args="${args} -DSTATIC_LINKING=YES"
@@ -84,39 +136,25 @@ if [ -n "$extra_args" ]
 then
     args="${args} ${extra_args}"
 fi
+if [ -n "$universal_linux_build" ]
+then
+    for arch in "32" "64"
+    do
+        args="${args} -DCMAKE_INSTALL_PREFIX=${DPP_MSBAYES_BASE_DIR}/install/m${arch}"
+        args="${args} -DCMAKE_C_FLAGS=-m${arch}"
+        export DPP_MSBAYES_BUILD_DIR="${DPP_MSBAYES_BASE_DIR}/build/m${arch}"
+        build_dpp_msbayes $args
+    done
+fi
+if [ -n "$install_prefix" ]
+then
+    args="${args} -DCMAKE_INSTALL_PREFIX=${install_prefix}"
+fi
 if [ -n "$universal_mac_build" ]
 then
     args="${args} -DCMAKE_C_FLAGS=\"-arch ppc -arch i386 -arch x86_64\""
 fi
 
 # check for build directory
-build_dir="${base_dir}/build"
-if [ -d "$build_dir" ]
-then
-    echo "ERROR: build directory '$build_dir' already exists."
-    echo "To reconfigure, please remove this directory and re-run this script."
-    exit 1
-else
-    mkdir "$build_dir"
-fi
-
-# configure make files and build
-cd "$build_dir"
-echo "Configuring make files..."
-echo "    ../ $args | xargs cmake"
-echo "../ $args" | xargs cmake || exit 1
-echo "Building..."
-make || exit 1
-if [ -n "$run_tests" ]
-then
-    echo "Building and running test suite..."
-    make check || exit 1
-fi
-if [ -n "$run_install" ]
-then
-    echo "Installing..."
-    make install || exit 1
-fi
-cd "$base_dir"
-echo "Done!"
-
+export DPP_MSBAYES_BUILD_DIR="${DPP_MSBAYES_BASE_DIR}/build"
+build_dpp_msbayes $args
