930f1a8a0a8a29e6345dcf82167ed3a20c08dc64
Jamie Oaks <joaks1@gmail.com>
2014-05-22 21:07:35 -0500

    Working on `build-gsl-linux.sh`.
    It is now up and running and will build 32 and 64 bit versions.

--- dev-tools/build-gsl-linux.sh
+++ dev-tools/build-gsl-linux.sh
@@ -44,39 +44,22 @@ do
      
     # copy source to new directory
     ARCH_DIR="${GSLVER}_${ARCH}"
+    INSTALL_DIR="${BUILD_DIR}/${ARCH}"
     cp -r "${GSLVER}" "$ARCH_DIR"
     cd "$ARCH_DIR"
      
-    # configure for architecture
-    # if [ "$ARCH" != "ppc64" ] ; then  # not sure why we have to do cross compile for ppc64 but not ppc...
-    #     env CC="$COMPILER" CFLAGS="-arch $ARCH $COMPFLAGS" LDFLAGS="-arch $ARCH" ./configure $CONFFLAGS --build=$ARCH
-    # else
-    env CC="$COMPILER" CFLAGS="-$ARCH" LDFLAGS="-$ARCH" ./configure $CONFFLAGS
-    # fi
+    env CC="$COMPILER" CFLAGS="-$ARCH" LDFLAGS="-$ARCH" ./configure $CONFFLAGS --prefix="$INSTALL_DIR"
      
     # compile for architecture
-    make clean ; nice make -j $COMPILETHREADS
+    make clean
+    nice make -j $COMPILETHREADS
+    make install
      
     echo
     echo
-    echo Copying ${ARCH} headers and binaries...
-    INCLUDE_DIR="${BUILD_DIR}/${ARCH}/include"
-    LIB_DIR="${BUILD_DIR}/${ARCH}/libs"
-    mkdir -p "$INCLUDE_DIR"
-    mkdir -p "$LIB_DIR"
-    cp `find ${ARCH_DIR} -name "*.h"` "$INCLUDE_DIR"
-    cp `find ${ARCH_DIR} -name "*.a"` "$LIB_DIR"
-     
-    echo
-    echo
-    echo ${ARCH} headers are in:
-    echo "    $INCLUDE_DIR"
-    
-    echo
-    echo
-    echo ${ARCH} static libraries:
-    file "${LIB_DIR}"/*.a
+    echo ${ARCH} headers and binaries are in:
+    echo "    $INSTALL_DIR"
 
     cd "$BASE_DIR"
-     
+
 done
